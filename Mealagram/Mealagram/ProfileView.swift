b74986071144015be96465aafab38d654e9188ea
Austin Beck <abeck@lunchbox.io>
2021-09-03 11:23:59 -0500

    adding features to profile view

---
+++ Mealagram/Mealagram/ProfileView.swift
@@ -0,0 +1,127 @@
+//
+//  ProfileView.swift
+//  Mealagram
+//
+//  Created by Austin Beck on 9/3/21.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    @Environment(\.presentationMode) var presentationMode
+    @State var profileName = ""
+    @State var profileDescription = ""
+
+    var body: some View {
+        VStack {
+            Button(action: {
+                        print("Round Action")
+                        }) {
+                        Text("+")
+                            .frame(width: 100, height: 100)
+                            .background(Color.gray)
+                            .clipShape(Circle())
+                    }
+            .font(.title)
+            .padding(5)
+            Text("Add Image")
+                .font(.system(size: 10))
+            VStack {
+                TextField("Full name", text: $profileName)
+                    .font(.system(size: 10))
+                    .padding(10)
+                Spacer()
+                TextField("Description..", text: $profileDescription)
+                    .font(.system(size: 10))
+                    .padding(5)
+                Spacer()
+            }
+            .frame(width: 250, height: 100)
+            .cornerRadius(6)
+            .overlay(
+                RoundedRectangle(cornerRadius: 6)
+                    .stroke(Color.gray, lineWidth: 1)
+                    )
+            AllergiesCell()
+            VStack(alignment: .leading) {
+                Text("Favorites")
+                    .font(.system(size: 15))
+                    .padding(.leading, 30)
+                    .padding(.top, 20)
+                Divider()
+                HStack {
+                    
+                }
+            }
+        }
+    }
+}
+
+struct ProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileView()
+    }
+}
+
+struct AllergiesCell: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Allergies")
+                .font(.system(size: 15))
+                .padding(.leading, 30)
+                .padding(.top, 10)
+            Divider()
+            HStack() {
+                Button(action: {
+                    
+                }) {
+                    Text("Nuts")
+                }
+                .padding(.leading, 10)
+                .padding(.trailing, 10)
+                .cornerRadius(6)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 6)
+                        .stroke(Color.gray, lineWidth: 1)
+                )
+                Button(action: {
+                    
+                }) {
+                    Text("Dairy")
+                }
+                .padding(.leading, 10)
+                .padding(.trailing, 10)
+                .cornerRadius(6)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 6)
+                        .stroke(Color.gray, lineWidth: 1)
+                )
+                Button(action: {
+                    
+                }) {
+                    Text("Shellfish")
+                }
+                .padding(.leading, 10)
+                .padding(.trailing, 10)
+                .cornerRadius(6)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 6)
+                        .stroke(Color.gray, lineWidth: 1)
+                )
+                Button(action: {
+                    
+                }) {
+                    Text("Eggs")
+                }
+                .padding(.leading, 10)
+                .padding(.trailing, 10)
+                .cornerRadius(6)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 6)
+                        .stroke(Color.gray, lineWidth: 1)
+                )
+            }
+            .padding(.leading, 10)
+        }
+    }
+}
