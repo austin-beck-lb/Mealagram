680a45409ef65d4f8e993ddb01fa50cb1cd983a5
Brandon Shaw <unobrandon@mac.com>
2021-09-03 13:38:05 -0400

    minor

--- Mealagram/Mealagram/ContentView.swift
+++ Mealagram/Mealagram/ContentView.swift
@@ -16,6 +16,7 @@ struct ContentView: View {
                 .font(.largeTitle)
                 .fontWeight(.bold)
                 .padding()
+                .padding(.top, 30)
             Spacer()
 
             Button(action: {
