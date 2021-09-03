5d25258ebc63e2e17b119fb14c66e672a31fc27a
Austin Beck <abeck@lunchbox.io>
2021-09-03 09:56:21 -0500

    Pushing project

---
+++ Mealagram/Shared/MealagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealagramApp.swift
+//  Shared
+//
+//  Created by Austin Beck on 9/3/21.
+//
+
+import SwiftUI
+
+@main
+struct MealagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
