d5886aa7e6ffea57c2140c2666e85ef9be7430e7
Brandon Shaw <unobrandon@mac.com>
2021-09-03 13:33:38 -0400

    minor

--- Mealagram/Mealagram/CustomStyles.swift
+++ Mealagram/Mealagram/CustomStyles.swift
@@ -7,8 +7,8 @@
 
 import SwiftUI
 
-struct ClickButtonStyle: ButtonStyle {
-    public func makeBody(configuration: ClickButtonStyle.Configuration) -> some View {
+struct MainButtonStyle: ButtonStyle {
+    public func makeBody(configuration: MainButtonStyle.Configuration) -> some View {
         configuration.label
             .opacity(configuration.isPressed ? 0.95 : 1)
             .scaleEffect(configuration.isPressed ? 0.95 : 1.0)
