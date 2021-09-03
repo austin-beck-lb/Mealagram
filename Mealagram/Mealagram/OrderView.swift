b979cde8ac7ab3d9b9e2df7d25fe4a6c138ffdef
Brandon Shaw <unobrandon@mac.com>
2021-09-03 17:34:40 -0400

    minor

--- Mealagram/Mealagram/OrderView.swift
+++ Mealagram/Mealagram/OrderView.swift
@@ -29,62 +29,62 @@ struct OrderView: View {
     
     var body: some View {
         NavigationLink(destination: ThankYouView(), tag: 1, selection: $action) {
-         EmptyView()
-        }
-
-        ScrollView() {
-            VStack {
-                ForEach(self.menu, id: \.id) { item in
-                    Button(action: {
-                        if self.selectedMenu.id == item.id {
-                            self.selectedMenu = MenuItem(name: "", image: "", price: "")
-                        } else {
-                            self.selectedMenu = item
+            ScrollView() {
+                VStack {
+                    ForEach(self.menu, id: \.id) { item in
+                        Button(action: {
+                            if self.selectedMenu.id == item.id {
+                                self.selectedMenu = MenuItem(name: "", image: "", price: "")
+                            } else {
+                                self.selectedMenu = item
+                            }
+                        }) {
+                            HStack(alignment: .center, spacing: 15) {
+                                Image(item.image)
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 65, height: 65, alignment: .center)
+                                    .padding(.leading)
+                                
+                                Text(item.name)
+                                    .font(.body)
+                                    .fontWeight(.medium)
+                                    .foregroundColor(.primary)
+                                
+                                Spacer()
+                                Text(item.price)
+                                    .font(.caption)
+                                    .fontWeight(.none)
+                                    .foregroundColor(.secondary)
+                                    .padding(.horizontal)
+                            }
+                            .foregroundColor(Color.primary)
+                            .frame(width: UIScreen.main.bounds.width - 40, height: 80)
+                            .background(Color("buttonColor"))
+                            .overlay(RoundedRectangle(cornerRadius: 15).strokeBorder(self.selectedMenu.id == item.id ? Color.blue.opacity(0.8) : Color.clear, lineWidth: 3))
+                            .cornerRadius(15)
+                            .padding(5)
                         }
-                    }) {
-                        HStack(alignment: .center, spacing: 15) {
-                            Image(item.image)
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: 65, height: 65, alignment: .center)
-                                .padding(.leading)
-                            
-                            Text(item.name)
-                                .font(.body)
-                                .fontWeight(.medium)
-                                .foregroundColor(.primary)
-                            
-                            Spacer()
-                            Text(item.price)
-                                .font(.caption)
-                                .fontWeight(.none)
-                                .foregroundColor(.secondary)
-                                .padding(.horizontal)
-                        }
-                        .foregroundColor(Color.primary)
-                        .frame(width: UIScreen.main.bounds.width - 40, height: 80)
-                        .background(Color("buttonColor"))
-                        .overlay(RoundedRectangle(cornerRadius: 15).strokeBorder(self.selectedMenu.id == item.id ? Color.blue.opacity(0.8) : Color.clear, lineWidth: 3))
-                        .cornerRadius(15)
-                        .padding(5)
+                        .buttonStyle(MainButtonStyle())
+                        .shadow(color: Color.black.opacity(0.2), radius: 12, x: 0, y: 6)
+                        .padding(.horizontal)
                     }
-                    .buttonStyle(MainButtonStyle())
-                    .shadow(color: Color.black.opacity(0.2), radius: 12, x: 0, y: 6)
-                    .padding(.horizontal)
-                }
-            }.padding(.top, 30)
-        }
-        .navigationTitle("Menu")
-        .navigationBarTitleDisplayMode(.inline)
-        .navigationBarItems(trailing:
-            Button(action: {
-                guard self.selectedMenu.name != "" else { return }
+                }.padding(.top, 30)
+            }
+            .navigationTitle("Menu")
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarItems(trailing:
+                Button(action: {
+                    guard self.selectedMenu.name != "" else { return }
 
-                UIImpactFeedbackGenerator(style: .rigid).impactOccurred()
-                self.action = 1
-            }) {
-                Text("Next")
-                    .foregroundColor(self.selectedMenu.name == "" ? .secondary : .blue)
-            })
+                    UIImpactFeedbackGenerator(style: .rigid).impactOccurred()
+                    self.action = 1
+                }) {
+                    Text("Next")
+                        .foregroundColor(self.selectedMenu.name == "" ? .secondary : .blue)
+                })
+        }
+
+       
     }
 }
