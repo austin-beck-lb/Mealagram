1f8223b45d9c101145301fe2016fd52ad8169cc2
Brandon Shaw <unobrandon@mac.com>
2021-09-03 11:39:46 -0400

    added fresh project

---
+++ Mealagram/Mealagram/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mealagram
+//
+//  Created by Brandon Shaw on 9/3/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Mealagram/Mealagram/MealagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealagramApp.swift
+//  Mealagram
+//
+//  Created by Brandon Shaw on 9/3/21.
+//
+
+import SwiftUI
+
+@main
+struct MealagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
